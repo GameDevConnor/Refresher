deff2f02d9b02e213f8ea8f01e44fe0653a50b5e
Connor McGrath <connorfrancismcgrath@outlook.com>
2026-03-15 18:11:39 -0400

    import lexical, emotion, chakra. Created first text box

--- refresher/src/components/Research.tsx
+++ refresher/src/components/Research.tsx
@@ -2,10 +2,7 @@ function Research() {
   return (
     <div className="research">
       <h1>Research</h1>
-      <p>
-        This is the research page. It contains information about the research
-        that has been conducted on the topic of refresher courses.
-      </p>
+      <p></p>
     </div>
   );
 }
--- refresher/src/components/Section.tsx
+++ refresher/src/components/Section.tsx
@@ -1,5 +1,66 @@
+import { Box } from "@chakra-ui/react";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { CodeNode } from "@lexical/code";
+import { css } from "@emotion/css";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+
 function Section() {
-  return <></>;
+  function onError(error: Error) {
+    console.error(error);
+  }
+
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme: {},
+    onError: onError,
+    nodes: [HeadingNode, QuoteNode, CodeNode],
+  };
+
+  return (
+    <Box>
+      <LexicalComposer initialConfig={initialConfig}>
+        <Box
+          className={css({
+            position: "relative",
+          })}
+        >
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className={css({
+                  height: 120,
+                  fontSize: 12,
+                  padding: 8,
+                  overflow: "auto",
+                  outline: "none",
+                  border: "1px solid black",
+                  borderRadius: "4px",
+                })}
+              />
+            }
+            placeholder={
+              <Box
+                className={css({
+                  position: "absolute",
+                  top: 8,
+                  left: 8,
+                  color: "#999",
+                })}
+              >
+                Enter some text...
+              </Box>
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          ></RichTextPlugin>
+        </Box>
+        <AutoFocusPlugin />
+      </LexicalComposer>
+    </Box>
+  );
 }
 
 export default Section;
